5a2a0519760456b44a2bae1a84fc1de804bc17b8
Moges <Moges.ash@gmail.com>
2020-09-11 15:54:26 +0200

    mail opmaak

--- mail/src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
+++ mail/src/main/java/be/vdab/mail/mailing/DefaultLidMailing.java
@@ -5,7 +5,10 @@ import be.vdab.mail.exceptions.KanMailNietZendenException;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
 
 @Component
 public class DefaultLidMailing implements LidMailing{
@@ -15,6 +18,15 @@ public class DefaultLidMailing implements LidMailing{
     }
     @Override public void stuurMailNaRegistratie(Lid lid, String ledenURL) {
         try {
+            var message = sender.createMimeMessage();
+            var helper = new MimeMessageHelper(message);
+            helper.setTo(lid.getEmailAdres());
+            helper.setSubject("Geregistreerd");
+            helper.setText("<h1>Je bent nu lid.</h1>Je nummer is:" + lid.getId(), true); sender.send(message); }
+        catch (MailException | MessagingException ex) {
+            throw new KanMailNietZendenException(ex);
+        }
+        /*try {
             var message = new SimpleMailMessage();
             message.setTo(lid.getEmailAdres());
             message.setSubject("Geregistreerd");
@@ -23,6 +35,6 @@ public class DefaultLidMailing implements LidMailing{
         }
         catch (MailException ex) {
             throw new KanMailNietZendenException(ex);
-        }
+        }*/
     }
 }
