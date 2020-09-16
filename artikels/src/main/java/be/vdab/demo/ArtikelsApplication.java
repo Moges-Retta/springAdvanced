e1dd1ebf8400ee37289878fe090c78bc6f8e40bf
Moges <Moges.ash@gmail.com>
2020-09-16 23:08:12 +0200

    solved

---
+++ artikels/src/main/java/be/vdab/demo/ArtikelsApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtikelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtikelsApplication.class, args);
+    }
+
+}
---
+++ artikels/src/test/java/be/vdab/demo/ArtikelsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtikelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
