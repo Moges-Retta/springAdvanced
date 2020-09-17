6866df1f42eec967fb5eaaa00b0d486d8c473f88
Moges <Moges.ash@gmail.com>
2020-09-17 22:03:21 +0200

    eigen validation

---
+++ validation/src/main/java/be/vdab/validation/Product.java
@@ -0,0 +1,30 @@
+package be.vdab.validation;
+
+import be.vdab.validation.constraints.Prijs;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+
+public class Product {
+    @Prijs
+    private BigDecimal aankoopPrijs;
+    @Prijs
+    private BigDecimal verkoopPrijs;
+
+    public BigDecimal getAankoopPrijs() {
+        return aankoopPrijs;
+    }
+
+    public void setAankoopPrijs(BigDecimal aankoopPrijs) {
+        this.aankoopPrijs = aankoopPrijs;
+    }
+
+    public BigDecimal getVerkoopPrijs() {
+        return verkoopPrijs;
+    }
+
+    public void setVerkoopPrijs(BigDecimal verkoopPrijs) {
+        this.verkoopPrijs = verkoopPrijs;
+    }
+}
